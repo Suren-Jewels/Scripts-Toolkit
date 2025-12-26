64ddb8df39200d5de8b758f5077d5f175cc75220
Suren Jewels <SurenJewelsPro@gmail.com>
2025-12-25 21:28:04 -0800

    Create variables.tf

---
+++ terraform-iac/aws-modules/security-groups/variables.tf
@@ -0,0 +1,59 @@
+variable "name" {
+  description = "Name of the security group"
+  type        = string
+}
+
+variable "description" {
+  description = "Description of the security group"
+  type        = string
+  default     = "Managed by Terraform"
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the security group will be created"
+  type        = string
+}
+
+variable "ingress_rules" {
+  description = "List of ingress rule objects"
+  type = list(object({
+    description      = optional(string)
+    from_port        = number
+    to_port          = number
+    protocol         = string
+    cidr_blocks      = optional(list(string))
+    ipv6_cidr_blocks = optional(list(string))
+    security_groups  = optional(list(string))
+  }))
+  default = []
+}
+
+variable "egress_rules" {
+  description = "List of egress rule objects"
+  type = list(object({
+    description      = optional(string)
+    from_port        = number
+    to_port          = number
+    protocol         = string
+    cidr_blocks      = optional(list(string))
+    ipv6_cidr_blocks = optional(list(string))
+    security_groups  = optional(list(string))
+  }))
+  default = [
+    {
+      description      = "Allow all outbound"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      security_groups  = []
+    }
+  ]
+}
+
+variable "tags" {
+  description = "Additional tags to apply"
+  type        = map(string)
+  default     = {}
+}
