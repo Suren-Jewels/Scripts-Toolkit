8a268a67fcdefa4cdacb1df83f56dda3c8263a19
Suren Jewels <SurenJewelsPro@gmail.com>
2025-12-25 22:41:16 -0800

    Create outputs.tf

---
+++ terraform-iac/azure-modules/vpn-gateway/outputs.tf
@@ -0,0 +1,11 @@
+output "vpn_gateway_id" {
+  value = azurerm_virtual_network_gateway.this.id
+}
+
+output "vpn_public_ip" {
+  value = azurerm_public_ip.vpn_public_ip.ip_address
+}
+
+output "vpn_gateway_name" {
+  value = azurerm_virtual_network_gateway.this.name
+}
