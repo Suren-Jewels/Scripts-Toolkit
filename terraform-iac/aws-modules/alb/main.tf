3c103ae419c6199219d176faf0cdfbade766caa4
Suren Jewels <SurenJewelsPro@gmail.com>
2025-12-25 22:47:37 -0800

    Create main.tf

---
+++ terraform-iac/aws-modules/alb/main.tf
@@ -0,0 +1,39 @@
+resource "aws_lb" "this" {
+  name               = var.name
+  internal           = var.internal
+  load_balancer_type = "application"
+  security_groups    = var.security_group_ids
+  subnets            = var.subnet_ids
+
+  tags = var.tags
+}
+
+resource "aws_lb_target_group" "this" {
+  name        = "${var.name}-tg"
+  port        = var.target_port
+  protocol    = var.target_protocol
+  vpc_id      = var.vpc_id
+  target_type = var.target_type
+
+  health_check {
+    path                = var.health_check_path
+    protocol            = var.health_check_protocol
+    interval            = var.health_check_interval
+    timeout             = var.health_check_timeout
+    healthy_threshold   = var.health_check_healthy_threshold
+    unhealthy_threshold = var.health_check_unhealthy_threshold
+  }
+
+  tags = var.tags
+}
+
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = var.listener_port
+  protocol          = var.listener_protocol
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+}
