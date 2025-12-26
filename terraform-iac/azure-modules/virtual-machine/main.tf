43d142562c5b6febd9e65c290afe9b3ee46dd0af
Suren Jewels <SurenJewelsPro@gmail.com>
2025-12-25 22:19:31 -0800

    Create main.tf

---
+++ terraform-iac/azure-modules/virtual-machine/main.tf
@@ -0,0 +1,31 @@
+resource "azurerm_linux_virtual_machine" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  size                = var.vm_size
+  admin_username      = var.admin_username
+
+  network_interface_ids = [
+    var.network_interface_id
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = var.os_disk_type
+    disk_size_gb         = var.os_disk_size_gb
+  }
+
+  source_image_reference {
+    publisher = var.image.publisher
+    offer     = var.image.offer
+    sku       = var.image.sku
+    version   = var.image.version
+  }
+
+  tags = var.tags
+}
