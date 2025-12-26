0c6d853e1884f5784e77943c17421477f5097e09
Suren Jewels <SurenJewelsPro@gmail.com>
2025-12-25 21:15:32 -0800

    Create variables.tf

---
+++ terraform-iac/aws-modules/vpc-networking/variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  description = "Base name for VPC resources"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
