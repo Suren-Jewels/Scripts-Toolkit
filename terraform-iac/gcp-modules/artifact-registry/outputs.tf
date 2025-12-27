1798cf92efb8185a87aa844238f46a59e40ea7cb
Suren Jewels <SurenJewelsPro@gmail.com>
2025-12-26 20:33:46 -0800

    Create outputs.tf

---
+++ terraform-iac/gcp-modules/artifact-registry/outputs.tf
@@ -0,0 +1,7 @@
+output "repository_id" {
+  value = google_artifact_registry_repository.this.repository_id
+}
+
+output "repository_self_link" {
+  value = google_artifact_registry_repository.this.self_link
+}
