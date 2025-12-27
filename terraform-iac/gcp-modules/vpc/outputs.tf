c1fc32c45dc5253b652ef8f6fbef50a026225a0f
Suren Jewels <SurenJewelsPro@gmail.com>
2025-12-26 20:05:11 -0800

    Create outputs.tf

---
+++ terraform-iac/gcp-modules/vpc/outputs.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = google_compute_network.this.id
+}
+
+output "vpc_self_link" {
+  value = google_compute_network.this.self_link
+}
+
+output "vpc_name" {
+  value = google_compute_network.this.name
+}
